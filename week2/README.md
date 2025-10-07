25873df48a69d507381b2b4132b06016c9a8c5e3
acyanbird <acyanbird@gmail.com>
2025-10-07 12:09:39 +0100

    update till cfe2

--- week2/README.md
+++ week2/README.md
@@ -1,2 +1,5 @@
 ## This week we include [Biological Computing in Python](https://mulquabio.github.io/MQB/notebooks/Python.html)
 
+For `MyExampleScript.py` this file defines a simple function named foo that calculates the square of any number passed to it. And show how to execute a code in one file.  
+`cfexercises1.py` define several functions to do the calculation. Originally this program does not output anything. But after the modification, it is more module like. In the main function it will test foo using different number, and get the user input variables.   
+`cfexercises2.py` also define functions to play with for and while loop. I explain every function inside the file. And all the function will run using given parameters.
--- week2/code/cfexercises2.py
+++ week2/code/cfexercises2.py
@@ -1,6 +1,7 @@
 
 ########################
 def hello_1(x):
+    # Print 'hello' for each multiple of 3
     for j in range(x):
         if j % 3 == 0:
             print('hello')
@@ -10,6 +11,7 @@ hello_1(12)
 
 ########################
 def hello_2(x):
+    # Print 'hello' for each multiple of 4 or 5
     for j in range(x):
         if j % 5 == 3:
             print('hello')
@@ -21,6 +23,7 @@ hello_2(12)
 
 ########################
 def hello_3(x, y):
+    # Print 'hello' for each number between x and y
     for i in range(x, y):
         print('hello')
     print(' ')
@@ -29,6 +32,7 @@ hello_3(3, 17)
 
 ########################
 def hello_4(x):
+    # Print 'hello' while x is not equal to 15
     while x != 15:
         print('hello')
         x = x + 3
@@ -38,11 +42,15 @@ hello_4(0)
 
 ########################
 def hello_5(x):
+    # Print 'hello' while x is less than 100
+    # Get initial x and increment x by 1 each iteration
     while x < 100:
         if x == 31:
+            # Print 'hello' 7 times if x equals 31
             for k in range(7):
                 print('hello')
         elif x == 18:
+            # print hello 1 time if x equals 18
             print('hello')
         x = x + 1
     print(' ')
@@ -51,6 +59,7 @@ hello_5(12)
 
 # WHILE loop with BREAK
 def hello_6(x, y):
+    # Print 'hello' while x is True and break when y equals 6
     while x: # while x is True
         print("hello! " + str(y))
         y += 1 # increment y by 1 
