bcc7b6c9400b61795446f4fd41b610d1de9541c6
acyanbird <acyanbird@gmail.com>
2025-10-28 16:51:34 +0000

    data r practical r until 3 pdfs

---
+++ week5/README.md
@@ -0,0 +1,6 @@
+## [Data Management and Visualization](https://mulquabio.github.io/MQB/notebooks/Data_R.html)
+
+### Firstly is practical `Body mass distributions`.
+
+We create a `PP_Dists.R` script read data from a csv file. For the first 3 pdf file of plots, we get group name of interaction. Then using them into for loop create plot for each interaction (by create a subset of each interaction), and using par combine them together. Histogram is used to display distribution.
+
---
+++ week5/code/PP_Dists.R
@@ -0,0 +1,65 @@
+require(tidyverse) 
+
+MyDF <- read.csv("../data/EcolArchives-E089-51-D1.csv", stringsAsFactors = FALSE)
+
+# Turn the feeding interaction column into a factor easy to classify
+MyDF$Type.of.feeding.interaction <- as.factor(MyDF$Type.of.feeding.interaction)
+
+# Convert mg to g and adjust mass values
+MyDF$Prey.mass[MyDF$Prey.mass.unit == "mg"] <- MyDF$Prey.mass[MyDF$Prey.mass.unit == "mg"] / 1000
+MyDF$Prey.mass.unit[MyDF$Prey.mass.unit == "mg"] <- "g"
+
+# Get list of feeding interactions
+feeding_interactions <- levels(MyDF$Type.of.feeding.interaction)
+num_interactions <- length(feeding_interactions)
+
+pdf(file = "../results/Pred_Subplots.pdf", width = 12, height = 11)
+
+par(mfcol = c(ceiling(num_interactions / 2), 2))  # Set up plotting area
+
+# Create a separate plot for each feeding interaction
+for (i in 1:num_interactions) {
+  interaction <- feeding_interactions[i]
+  subset_df <- filter(MyDF, Type.of.feeding.interaction == interaction)
+
+
+  # Basic scatter plot with log10 axes
+    hist(log10(subset_df$Predator.mass),xlab="log10(Predator Mass (g))",ylab="Count", 
+    col = "lightblue", border = "#ab3044") # Change bar and borders colors 
+}
+
+graphics.off()
+
+pdf(file = "../results/Prey_Subplots.pdf", width = 12, height = 11)
+
+par(mfcol = c(ceiling(num_interactions / 2), 2))  # Set up plotting area
+
+# Create a separate plot for each feeding interaction
+for (i in 1:num_interactions) {
+  interaction <- feeding_interactions[i]
+  subset_df <- filter(MyDF, Type.of.feeding.interaction == interaction)
+
+
+  # Basic scatter plot with log10 axes
+    hist(log10(subset_df$Prey.mass),xlab="log10(Prey Mass (g))",ylab="Count", 
+    col = "lightblue", border = "#b8384d") # Change bar and borders colors 
+}
+
+graphics.off()
+
+pdf(file = "../results/SizeRatio_Subplots.pdf", width = 12, height = 11)
+
+par(mfcol = c(ceiling(num_interactions / 2), 2))  # Set up plotting area
+
+# Create a separate plot for each feeding interaction
+for (i in 1:num_interactions) {
+  interaction <- feeding_interactions[i]
+  subset_df <- filter(MyDF, Type.of.feeding.interaction == interaction)
+
+
+  # Basic scatter plot with log10 axes
+    hist(log10(subset_df$Prey.mass/subset_df$Predator.mass),xlab="log10(Prey Mass / Predator Mass)",ylab="Count", 
+    col = "lightblue", border = "#ac2e43") # Change bar and borders colors 
+}
+
+graphics.off()
