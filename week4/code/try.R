1fc791e285a9cc7e088666acea5f09032d377b63
acyanbird <acyanbird@gmail.com>
2025-12-11 17:18:55 +0000

    fix try r

--- week4/code/try.R
+++ week4/code/try.R
@@ -2,7 +2,8 @@ doit <- function(x) {
   #
   temp_x <- sample(x, replace = TRUE)
   if(length(unique(temp_x)) > 30) {#only take mean if sample was sufficient
-    #print(paste("Mean of this sample was:", as.character(mean(temp_x))))
+    print(paste("Mean of this sample was:", as.character(mean(temp_x))))
+    return(mean(temp_x))
   } 
   else {
     stop("Couldn't calculate mean: too few unique values!")
@@ -15,10 +16,9 @@ popn <- rnorm(50) # 50 random numbers
 
 hist(popn)
 
-# try(, silent = FALSE)  print error messages
-result <- lapply(1:15, function(i) try(doit(popn)))
+result <- lapply(1:15, function(i) try(doit(popn), silent = TRUE)) #suppress error messages
 
 result <- vector("list", 15) #Preallocate/Initialize
 for(i in 1:15) {
-  result[[i]] <- try(doit(popn))
+  result[[i]] <- try(doit(popn), silent = TRUE)
 }
